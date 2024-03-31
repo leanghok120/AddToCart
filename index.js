779caae812e6eae7bef316777df3070115147215
leanghok120 <leanghokoeng5@gmail.com>
2024-04-01 04:58:15 +0700

    Add files

---
+++ index.js
@@ -0,0 +1,8 @@
+const addBtn = document.getElementById("add-button")
+const inputBar = document.getElementById("input-field")
+
+addBtn.addEventListener("click", function() {
+  console.log(inputBar.value)
+})
+
+
