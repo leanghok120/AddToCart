927a6b02d746c1ba5299c8a3c7aed8a833ff08a5
leanghok120 <leanghokoeng5@gmail.com>
2024-04-02 19:41:23 +0700

    Clear the input field when the add cart button is pressed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 const appSettings = {
   databaseURL: "https://realtime-database-90f99-default-rtdb.asia-southeast1.firebasedatabase.app/" 
@@ -13,7 +14,12 @@ const shoppingList = ref(db, "shoppingList")
 
 addButtonEl.addEventListener("click", function() {
   let inputValue = inputFieldEl.value
+
   push(shoppingList, inputValue)
-  
+
   console.log(inputValue + " is added to database")
+  shoppingListEl.innerHTML += `<li>${inputValue}</li>` 
+  
+  inputFieldEl.value = ""
+  console.log("Clear input-field")
 })
