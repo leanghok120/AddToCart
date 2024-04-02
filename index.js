83eb85b0e30f8fe8dda86ee8e74ac09cfa0ca73c
leanghok120 <leanghokoeng5@gmail.com>
2024-04-02 19:27:57 +0700

    Add firebase db to project

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
+const appSettings = {
+  databaseURL: "https://realtime-database-90f99-default-rtdb.asia-southeast1.firebasedatabase.app/" 
+}
+const app = initializeApp(appSettings)
+const db = getDatabase(app)
+const shoppingList = ref(db, "shoppingList")
+
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    console.log(inputValue)
+  let inputValue = inputFieldEl.value
+  push(shoppingList, inputValue)
+  
+  console.log(inputValue + " is added to database")
 })
